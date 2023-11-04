44d83f6e6f10cd895e29ef06e3a891687172eb08
Petr <petr.mvd+git@gmail.com>
2023-11-04 12:49:59 +0100

    Remove gerbers and files that have been moved to `hw/`
    Update .gitignore

--- sw/main.cc
+++ sw/main.cc
@@ -1,8 +1,12 @@
+#include <avr/eeprom.h>
 #include <avr/io.h>
+#include <stdint.h>
 #include <util/delay.h>
 
-int main(void)
-{
-    _delay_ms(1000);
-    return 0;
+uint8_t EEMEM twi_address = 18; // Randomly generated - https://xkcd.com/221/
+
+int main(void) {
+  eeprom_write_byte(&twi_address, 42);
+  _delay_ms(1000);
+  return 0;
 }
