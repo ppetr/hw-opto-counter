5dac5203618910bdcd5e864e7c28ab06c38fa901
Petr <petr.mvd+git@gmail.com>
2023-11-24 23:52:20 +0100

    Configure the TCA0 to emit a test PWM signal.

--- sw/main.cc
+++ sw/main.cc
@@ -16,15 +16,40 @@ extern "C" {
 
 #include <avr/eeprom.h>
 #include <avr/io.h>
+#include <avr/sleep.h>
 #include <stdint.h>
 #include <util/delay.h>
 
 }  // extern "C"
 
-uint8_t EEMEM twi_address = 18;  // Randomly generated - https://xkcd.com/221/
+// uint8_t EEMEM twi_address = 18;  // Randomly generated -
+// https://xkcd.com/221/
+
+class TCA0_PWM {
+ public:
+  TCA0_PWM(float /*freq*/, float /*duty_cycle*/)
+      : prev_ctrla_(TCA0.SINGLE.CTRLA), prev_ctrlb_(TCA0.SINGLE.CTRLB) {
+    TCA0.SINGLE.PER = 3255;
+    TCA0.SINGLE.CMP0 = 3000;
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP0EN_bm;
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1024_gc | TCA_SINGLE_ENABLE_bm;
+  }
+  ~TCA0_PWM() { TCA0.SINGLE.CTRLA = prev_ctrla_; }
+
+ private:
+  const uint8_t prev_ctrla_;
+  const uint8_t prev_ctrlb_;
+};
 
 int main(void) {
-  eeprom_read_byte(&twi_address);
-  _delay_ms(1000);
-  return 0;
+  // eeprom_read_byte(&twi_address);
+  // Enable the TCA0 WO0 (PB0) pin.
+  PORTB.DIRSET = PIN0_bm;
+  TCA0_PWM pwm(1.0, 0.25);
+
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
+  while (true) {
+    sleep_mode();
+  }
 }
