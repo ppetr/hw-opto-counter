8b9362457d958b868b1fa71a0cdd5c75554625a4
Petr <petr.mvd+git@gmail.com>
2023-11-25 14:26:12 +0100

    Configure the TCA0 counter using `constexpr` precomputed register values

--- sw/main.cc
+++ sw/main.cc
@@ -18,7 +18,7 @@ extern "C" {
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <stdint.h>
-#include <util/delay.h>
+#include <stdlib.h>
 
 }  // extern "C"
 
@@ -27,25 +27,68 @@ extern "C" {
 
 class TCA0_PWM {
  public:
-  TCA0_PWM(float /*freq*/, float /*duty_cycle*/)
-      : prev_ctrla_(TCA0.SINGLE.CTRLA), prev_ctrlb_(TCA0.SINGLE.CTRLB) {
-    TCA0.SINGLE.PER = 3255;
-    TCA0.SINGLE.CMP0 = 3000;
-    TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP0EN_bm;
-    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1024_gc | TCA_SINGLE_ENABLE_bm;
-  }
-  ~TCA0_PWM() { TCA0.SINGLE.CTRLA = prev_ctrla_; }
+  struct Config {
+    // Finds which divider value will fit in the 16-bit counter with maximum
+    // precision. A `constexpr` constructor allows to do all floating point
+    // computations at compile time.
+    constexpr explicit Config(float freq)
+        : clkSel(ClkSelFor(freq)),
+          per(static_cast<uint16_t>(kClkSelFreq[clkSel] / freq - 1)) {}
 
- private:
-  const uint8_t prev_ctrla_;
-  const uint8_t prev_ctrlb_;
+    // The clock prescaler selection 0-7.
+    uint8_t clkSel;
+    // The TOP counter value.
+    uint16_t per;
+
+   private:
+    constexpr static uint8_t ClkSelFor(float freq) {
+      uint8_t clkSel = 0;
+      for (; clkSel < 8; clkSel++) {
+        if (kClkSelFreq[clkSel] / 65536.0 < freq) {
+          break;
+        }
+      }
+      return clkSel;
+    }
+    constexpr static float kClkSelFreq[] = {
+        float{F_CPU} / 1.0,   float{F_CPU} / 2.0,   float{F_CPU} / 4.0,
+        float{F_CPU} / 8.0,   float{F_CPU} / 16.0,  float{F_CPU} / 64.0,
+        float{F_CPU} / 256.0, float{F_CPU} / 1024.0};
+  };
+
+  // Sets up the PWM, but with 0 duty cycle.
+  TCA0_PWM(Config freq) {
+    // See Section 21.5.1 in the manual.
+    PORTMUX.TCAROUTEA = 0;
+    TCA0.SINGLE.PER = freq.per;
+    TCA0.SINGLE.CMP0 = 0;  // Duty cycle.
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP0EN_bm;
+    // Enable last.
+    TCA0.SINGLE.CTRLA =
+        ((freq.clkSel << TCA_SINGLE_CLKSEL_gp) & TCA_SINGLE_CLKSEL_gm) |
+        TCA_SINGLE_ENABLE_bm;
+  }
+  ~TCA0_PWM() {
+    TCA0.SINGLE.CTRLA = 0;  // Disable completely.
+  }
+
+  // `duty_cycle` from 0 (static low) to 2^14 (static high).
+  void SetDutyCycle(uint16_t duty_cycle_14bit) {
+    TCA0.SINGLE.CMP0 = static_cast<uint16_t>(
+        ((long{TCA0.SINGLE.PER} + 1) * duty_cycle_14bit) >> 14);
+  }
 };
+
+constexpr float TCA0_PWM::Config::kClkSelFreq[];
+
+constexpr const TCA0_PWM::Config kLedPwmFreq(1.0);
 
 int main(void) {
   // eeprom_read_byte(&twi_address);
-  // Enable the TCA0 WO0 (PB0) pin.
+  // Enable the TCA0 PA5 pin.
   PORTB.DIRSET = PIN0_bm;
-  TCA0_PWM pwm(1.0, 0.25);
+  TCA0_PWM pwm(kLedPwmFreq);
+  pwm.SetDutyCycle(3 << 12);
 
   set_sleep_mode(SLEEP_MODE_IDLE);
   sleep_enable();
