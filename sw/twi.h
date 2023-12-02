91c0ffa6b482ceb54f59bf5e4bc8a50fc016eb00
Petr <petr.mvd+git@gmail.com>
2023-12-02 20:28:54 +0100

    Clean up the TWI implementation
    Ensure that all paths set the `SCTRLB` register value by expressing them
    using a function with a return value.

--- sw/twi.h
+++ sw/twi.h
@@ -38,8 +38,7 @@ class Twi {
     TWI0.SADDR = address << 1;
     TWI0.SADDRMASK = 0;
     // Standard or regular fast mode.
-    TWI0.SCTRLA =
-        TWI_ENABLE_bm | TWI_DIEN_bm | TWI_PIEN_bm | TWI_APIEN_bm | TWI_SMEN_bm;
+    TWI0.SCTRLA = TWI_ENABLE_bm | TWI_DIEN_bm | TWI_PIEN_bm | TWI_APIEN_bm;
   }
   ~Twi() { TWI0.SCTRLA = 0; }
   Twi(const Twi&) = delete;
@@ -47,48 +46,49 @@ class Twi {
 
   void Publish(const ReadData& read_data) { read_data_ = read_data; }
 
-  void OnInterrupt() {
+  void OnInterrupt() { TWI0.SCTRLB = OnInterrupt(TWI0.SSTATUS); }
+
+ private:
+  // Returns the value to be written into `SCTRLB`.
+  uint8_t OnInterrupt(uint8_t status) {
+    // Writing a TWI_SCMD... command to SCTRLB clears TWI_DIF and TWI_APIF.
     constexpr static uint8_t kTwiDirHostRead = TWI_DIR_bm;
-    const uint8_t status = TWI0.SSTATUS;
     if (status & TWI_BUSERR_bm) {
       // Clear the flag and wait for the protocol to restart.
       TWI0.SSTATUS = TWI_BUSERR_bm;
-      return;
-    }
-    if (status & TWI_COLL_bm) {  // The flag will be cleared automatically.
+      return TWI_SCMD_NOACT_gc;
+    } else if (status &
+               TWI_COLL_bm) {  // The flag will be cleared automatically.
       // TODO: Reset the internal state.
-      return;
-    }
-    // Writing a TWI_SCMD... command to SCTRLB clears TWI_DIF and TWI_APIF.
-    // Also Smart Mode (TWI_SMEN_bm) resets TWI_DIF on accessing SDATA.
-    if (status & TWI_APIF_bm) {
+      return TWI_SCMD_NOACT_gc;
+    } else if (status & TWI_APIF_bm) {  // Address or stop interrupt.
       if ((status & TWI_AP_bm) == TWI_AP_ADR_gc) {  // Address.
         read_snapshot_ = read_data_;
         read_index_ = 0;
-        TWI0.SCTRLB = TWI_ACKACT_ACK_gc | TWI_SCMD_RESPONSE_gc;
-        return;
+        return TWI_ACKACT_ACK_gc | TWI_SCMD_RESPONSE_gc;
       } else {  // Stop.
-        TWI0.SCTRLB = TWI_ACKACT_ACK_gc | TWI_SCMD_COMPTRANS_gc;
-        return;
+        return TWI_ACKACT_ACK_gc | TWI_SCMD_COMPTRANS_gc;
       }
-    }
-    if (status & TWI_DIF_bm) {
+    } else if (status & TWI_DIF_bm) {  // Data interrupt.
       if ((status & TWI_DIR_bm) == kTwiDirHostRead) {
         if (read_index_ < sizeof(read_snapshot_)) {
           TWI0.SDATA =
               reinterpret_cast<const uint8_t*>(&read_snapshot_)[read_index_++];
+          return TWI_ACKACT_ACK_gc | TWI_SCMD_RESPONSE_gc;
         } else {
-          TWI0.SCTRLB = TWI_ACKACT_NACK_gc | TWI_SCMD_RESPONSE_gc;
+          return TWI_ACKACT_NACK_gc | TWI_SCMD_COMPTRANS_gc;
         }
-      } else {  // Write.
+      } else {  // Host write.
         // Swallow up any incoming data.
         (void)TWI0.SDATA;
         // TODO: Use this to receive a register number to read from.
+        return TWI_ACKACT_ACK_gc | TWI_SCMD_RESPONSE_gc;
       }
+    } else {
+      return TWI_SCMD_NOACT_gc;
     }
   }
 
- private:
   ReadData read_data_;
   // Currently being read by `OnInterrupt`.
   ReadData read_snapshot_;
